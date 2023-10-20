ab58992dfd97076d821c78b8ed2b26f297e6f72c
Wum1ng <1210078968@qq.com>
2023-10-20 14:37:23 +0800

    delete *.swp .discord code

--- dojo_plugin/pages/course.py
+++ dojo_plugin/pages/course.py
@@ -9,8 +9,7 @@ from CTFd.utils.user import get_current_user, is_admin
 from CTFd.utils.decorators import authed_only, admins_only
 from CTFd.cache import cache
 
-from .discord import get_discord_user
-from ..models import DiscordUsers, DojoChallenges, DojoUsers, DojoStudents, DojoModules, DojoStudents
+from ..models import DojoChallenges, DojoUsers, DojoStudents, DojoModules, DojoStudents
 from ..utils import module_visible, module_challenges_visible, DOJOS_DIR, is_dojo_admin
 from ..utils.dojo import dojo_route
 from .writeups import WriteupComments, writeup_weeks, all_writeups
